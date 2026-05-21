b1c5d88093d27ba03a59c0de098708ce6ea53070
Keiton <syukatsukeito@gmail.com>
2026-05-21 17:47:12 +0900

    Implement basic login

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,8 @@ threads min_threads_count, max_threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
-preload_app!
-plugin :tmp_restart
+workers_count = ENV.fetch("WEB_CONCURRENCY") { 0 }.to_i
+workers workers_count if workers_count.positive?
+preload_app! if workers_count.positive?
+plugin :tmp_restart unless Gem.win_platform?
 
